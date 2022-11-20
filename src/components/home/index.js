066fb2a65c20faa55f7ebd737b2b527eecfc6d03
Jayesh Lalwani <lalwanijayesh09@gmail.com>
2022-11-19 23:25:56 -0500

    Implement likes toggle feature

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -68,7 +68,8 @@ const Home = () => {
           </div>
         }
       </div>
-      <Tuits tuits={tuits} deleteTuit={deleteTuit}/>
+      <Tuits tuits={tuits} deleteTuit={deleteTuit}
+             refreshTuits={findTuits}/>
     </div>
   );
 };
--- src/components/tuits/index.js
+++ src/components/tuits/index.js
@@ -1,15 +1,25 @@
 import React from "react";
 import './tuits.css';
 import Tuit from "./tuit";
+import * as service from "../../services/likes-service";
 
-function Tuits({tuits = [], deleteTuit}) {
+function Tuits({tuits = [], deleteTuit, refreshTuits}) {
+
+    const likeTuit = (tuit) =>
+        service.userTogglesTuitLikes("me", tuit._id)
+            .then(refreshTuits)
+            .catch(e => alert(e));
+
     return (
     <div>
       <ul className="ttr-tuits list-group">
         {
           tuits.map && tuits.map(tuit => {
             return (
-              <Tuit key={tuit._id} deleteTuit={deleteTuit} tuit={tuit}/>
+              <Tuit key={tuit._id}
+                    deleteTuit={deleteTuit}
+                    likeTuit={likeTuit}
+                    tuit={tuit}/>
             );
           })
         }
