37fccea6caf9ddfeb8f07e72913ccdf62d84a9dd
Jayesh Lalwani <lalwanijayesh09@gmail.com>
2022-11-20 03:09:37 -0500

    Add dislikes button and service

--- src/components/tuits/index.js
+++ src/components/tuits/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import './tuits.css';
 import Tuit from "./tuit";
-import * as service from "../../services/likes-service";
+import * as likesService from "../../services/likes-service";
+import * as dislikesService from "../../services/dislikes-service";
 
 function Tuits({tuits = [], deleteTuit, refreshTuits}) {
 
     const likeTuit = (tuit) =>
-        service.userTogglesTuitLikes("me", tuit._id)
+        likesService.userTogglesTuitLikes("me", tuit._id)
+            .then(refreshTuits)
+            .catch(e => alert(e));
+
+    const dislikeTuit = (tuit) =>
+        dislikesService.userTogglesTuitDislikes("me", tuit._id)
             .then(refreshTuits)
             .catch(e => alert(e));
 
@@ -19,6 +25,7 @@ function Tuits({tuits = [], deleteTuit, refreshTuits}) {
               <Tuit key={tuit._id}
                     deleteTuit={deleteTuit}
                     likeTuit={likeTuit}
+                    dislikeTuit={dislikeTuit}
                     tuit={tuit}/>
             );
           })
